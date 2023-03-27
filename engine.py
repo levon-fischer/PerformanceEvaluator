683e89d9189736dcd764d0302a7f86c5ea2dbc60
levon-fischer <levon.fischer@gmail.com>
2023-03-26 22:05:41 -0400

    Create initial version of the engine

---
+++ engine.py
@@ -0,0 +1,201 @@
+import openai
+import os
+import io
+import pandas as pd
+import logging
+
+class EvaluatorEngine:
+    """
+    The main class of the Evaluator engine. It stores the database and application parameters, as well as
+    coordinates the calls to GPT-3 model, leveraging the preprocessor and postprocessor. In this manner,
+    it provides the capability both to insert new statements and to query the database.
+    """
+
+    def __init__(self, api_key = os.getenv("OPENAI_API_KEY"),
+                 database_file_path = './data/default_database.csv',
+                 gpt_engine = 'text-davinci-003',
+                 gpt_temperature=0.1):
+        self._database_file_path = database_file_path
+
+        # Load the database or create it from scratch if needed
+        try:
+            self.database = pd.read_csv(self._database_file_path)
+            logging.info(f'Loaded database from {self._database_file_path}.')
+        except FileNotFoundError:
+            self.database = pd.DataFrame(columns=['statement', 'tier', 'award', 'category', 'score'])
+            self._save()
+            logging.info(f'Created database in {self._database_file_path}')
+
+
+        openai.api_key = api_key
+        self.gpt_parameters = {'engine': gpt_engine,
+                               'temperature': gpt_temperature,
+                               'max_tokens': 1000,
+                               'top_p': 1.0,
+                               'frequency_penalty': 0.0,
+                               'presence_penalty': 0.0,
+                               'stop': None}
+
+        self._current_extracted_statement = None
+
+        # Create preprocessor and postprocessor for GPT inputs and outputs
+        self._preprocessor = StatementPreprocessor()
+        self._postprocessor = StatementPostprocessor()
+
+    def _save(self):
+        logging.info(f'Database has {len(self.database)} facts.')
+
+        self.database.to_csv(self._database_file_path, index=False)
+
+        logging.info(f'Saved database in {self._database_file_path}.')
+
+    ####################################
+    # Facts insertion workflow methods #
+    ####################################
+
+    def extract_statement(self, statement_utterance):
+        """
+        Extracts facts from a natural language utterance. Returns a list of tuples (statement, tier, award, category, score).
+        """
+        statement_tuples = self._postprocessor.string_to_tuples(self._gpt_complete(self._preprocessor.extraction_prompt(statement_utterance)))
+        self._current_extracted_statement = statement_tuples
+        return statement_tuples
+
+    def has_extracted_statement(self):
+        return self._current_extracted_statement is not None
+
+    def commit(self):
+        """
+        Commits the current statement to the database. If no statement has been extracted,
+        the method just does nothing.
+        """
+        if self._current_extracted_statement is not None:
+            self._insert_statement()
+            self._current_extracted_statement = None
+            self._save()
+        else:
+            logging.info("Nothing to commit.")
+
+    def cancel(self):
+        """
+        Cancel the current extracted facts. If no facts have been extracted, the method just does nothing.
+        """
+        if self._current_extracted_statement is not None:
+            self._current_extracted_statement = None
+        else:
+            logging.info('Nothing to revert')
+
+    def _insert_statement(self, statement_utterance = None):
+        """
+        Inserts a statement into the database.
+        """
+
+        # reuse the extracted statement, if any
+        if self._current_extracted_statement is None:
+            statement_tuples = self.extract_statement(statement_utterance)
+        else:
+            statement_tuples = self._current_extracted_statement
+
+        for statement_tuple in statement_tuples:
+            logging.info(f'Database has {len(self.database)} statements before insertion.')
+            logging.info(f'Inserting statement: {statement_tuple}')
+
+            df_to_add = pd.DataFrame([statement_tuple], columns=['statement', 'tier', 'award', 'category', 'score'])
+            self.database = pd.concat([self.database, df_to_add], ignore_index=True)
+
+            logging.info(f'Database has {len(self.database)} statements after insertion.')
+
+    ###########
+    # GPT API #
+    ###########
+
+    def _gpt_complete(self, prompt, echo=False):
+
+        response = openai.Completion.create(
+            engine = self.gpt_parameters['engine'],
+            prompt = prompt,
+            temperature = self.gpt_parameters['temperature'],
+            max_tokens = self.gpt_parameters['max_tokens'],
+            top_p = self.gpt_parameters['top_p'],
+            frequency_penalty = self.gpt_parameters['frequency_penalty'],
+            presence_penalty = self.gpt_parameters['presence_penalty'],
+            stop = self.gpt_parameters['stop'],
+            echo = echo
+        )
+
+        completion = response['choices']['0']['text']
+
+        return completion
+
+    def set_openai_api_key(self, key):
+        openai.api_key = key
+
+    ##################
+    # Data utilities #
+    ##################
+
+    def export_data_to_binary(self, df, file_type=None):
+        if file_type is None:
+            file_type = 'excel'
+
+        if file_type == 'excel':
+            memory_output = io.BytesIO()
+            with pd.ExcelWriter(memory_output) as writer:
+                df.to_excel(writer)
+            return memory_output
+
+        elif file_type == 'csv':
+            return df.to_csv().encode('utf-8')
+
+        elif file_type == 'tsv':
+            return df.to_csv(sep='\t').encode('utf-8')
+
+        else:
+            return ValueError('Invalid file type.')
+
+class StatementPreprocessor:
+    """
+    Preprocessor for the user input to GPT. Notably, includes the mechanisms to build prompts.
+    """
+
+    def extraction_prompt(self, x, tier, award, category):
+
+        prompt = \
+f"""
+You are a panel member for an Air Force awards board. Read the following performace statement and grade it on a scale of 1 to 10.
+ - Tier: {tier}
+ - Award type: {award}
+ - Statement Category: {category}
+ 
+Example input: "blah blah blah"
+Example output: "blah blah blah
+                score: 8/10"
+
+Example input: "blah blah blah"
+Example output: "blah blah blah
+                score: 8/10"
+
+Input: {x}
+Output: 
+
+"""
+        logging.info(f'GPT Prompt: {prompt}')
+        return prompt
+
+class StatementPostprocessor:
+    """
+    Postprocessor for the GPT raw outputs.
+    """
+
+    def extract_score_from_result(self, result):
+        """
+        Extracts the score from the result string
+        """
+        score = result[-6:]
+        return score
+
+    def string_to_tuples(self, s):
+        """
+        Converts a string that looks like a tuple to an actual Python tuple.
+        """
+        return [eval(s.strip()) for s in self.extract_score_from_result(s)]
